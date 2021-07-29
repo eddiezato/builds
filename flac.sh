bc61f66c5582057d5bfcf708b2d3c0174f160725
eddie.zato <eddie.zato@gmail.com>
2021-07-29 14:12:13 +0700

    Add files via upload

--- flac.sh
+++ flac.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 export CC=clang CXX=clang++
+rm -rf flac
 git clone https://github.com/xiph/flac.git --recursive
 cd flac
-git pull
-rm -rf build
-cmake -B build -G Ninja -S ./ \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DWITH_OGG=OFF \
-    -DBUILD_EXAMPLES=OFF \
-    -DBUILD_DOCS=OFF \
-    -DINSTALL_MANPAGES=OFF \
-    -DCMAKE_C_FLAGS='-ffunction-sections -fdata-sections' \
-    -DCMAKE_CXX_FLAGS='-ffunction-sections -fdata-sections' \
-    -DCMAKE_EXE_LINKER_FLAGS='-Wl,--gc-sections -Wl,--no-export-dynamic -fstack-protector-strong -static'
-ninja -C build
+./autogen.sh
+CFLAGS='-ffunction-sections -fdata-sections' \
+CXXFLAGS='-ffunction-sections -fdata-sections' \
+LDFLAGS='-Wl,--gc-sections -Wl,--no-export-dynamic -fstack-protector-strong -lssp' \
+./configure --disable-shared --enable-static --disable-ogg --enable-64-bit-words --disable-doxygen-docs --disable-xmms-plugin --disable-cpplibs --disable-examples
+make -j 4
+strip -s src/flac/flac.exe
