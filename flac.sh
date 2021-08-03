fcd1e085021ea0c00aabf636563418f32aecbd44
eddie.zato <eddie.zato@gmail.com>
2021-08-03 21:33:03 +0700

    Add files via upload

--- flac.sh
+++ flac.sh
@@ -4,9 +4,17 @@ rm -rf flac
 git clone https://github.com/xiph/flac.git --recursive
 cd flac
 ./autogen.sh
-CFLAGS='-ffunction-sections -fdata-sections' \
-CXXFLAGS='-ffunction-sections -fdata-sections' \
-LDFLAGS='-Wl,--gc-sections -Wl,--no-export-dynamic -fstack-protector-strong -lssp' \
-./configure --disable-shared --enable-static --disable-ogg --enable-64-bit-words --disable-doxygen-docs --disable-xmms-plugin --disable-cpplibs --disable-examples
+CPPFLAGS="-D_FORTIFY_SOURCE=0" \
+CFLAGS="-march=native -O3 -fno-common -fno-plt -fno-stack-protector -fno-math-errno -fno-trapping-math -pipe" \
+CXXFLAGS="$CFLAGS" \
+LDFLAGS="-Wl,-O1,--sort-common,--as-needed ${CFLAGS}" \
+./configure --disable-shared \
+            --enable-static \
+            --disable-ogg \
+            --disable-xmms-plugin \
+            --disable-rpath \
+            --enable-64-bit-words \
+            --disable-stack-smash-protection \
+            --disable-examples
 make -j 4
 strip -s src/flac/flac.exe
