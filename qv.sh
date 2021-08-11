59e62e5a95322f796d7e326a6a848e3be2701883
eddie.zato <eddie.zato@gmail.com>
2021-08-11 17:46:14 +0700

    Add files via upload

---
+++ qv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export CC=clang CXX=clang++
+rm -rf quickviewer
+git clone https://github.com/kanryu/quickviewer.git --recursive
+rm -rf ~/quickviewer/Qt7z/Qt7z/7zip/{Asm,C,CPP,DOC}
+curl -#O https://www.7-zip.org/a/7z2103-src.7z
+7z x 7z2103-src.7z -oquickviewer/Qt7z/Qt7z/7zip -y
+rm -f 7z2103-src.7z
+cd quickviewer
+git apply ~/qv.patch
+git apply ~/qv7z.patch
+mkdir build
+cd build
+qmake "CONFIG += optimize_full" -o Makefile -recursive ../QVProject.pro
+make -j 4
