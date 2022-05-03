9e54f9db813628102bd224eca1b1d3ba7fdc88b7
eddie.zato <eddie.zato@gmail.com>
2022-05-03 10:57:45 +0700

    Delete qjxl.sh

--- qjxl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export CC=clang CXX=clang++
-rm -rf qt-jpegxl-image-plugin
-git clone --depth 1 https://github.com/novomesk/qt-jpegxl-image-plugin.git
-cd qt-jpegxl-image-plugin
-printf '\nQMAKE_CFLAGS *= \"-march=native -pipe\"\nQMAKE_CXXFLAGS *= \"-march=native -pipe\"' >> qt-jpegxl-image-plugin.pro
-qmake "CONFIG += optimize_full" qt-jpegxl-image-plugin.pro
-make -j 4
